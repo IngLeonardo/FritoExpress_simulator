c212847af80ef3904dad6d934bc00c96da3fcfdc
IngLeonardo <8471301+IngLeonardo@users.noreply.github.com>
2024-04-02 23:29:11 -0500

    no logro manipular los datos del objeto que esta del arreglo

--- calcular.js
+++ calcular.js
@@ -1,22 +1,78 @@
-//--- ARREGLO PARA IMPRIMIR EN PANTALLA LOS PRODUCTOS
-    let listaProductos = ['Papas de pollo',
-                        'Papas de limon',
-                        'Papas de mayonesa',
-                        'Panecillos 10gr',
-                        'chetos de queso',
-                        'Coca-Cola 350ml',
-                        'Pepsi 350ml',
-                        'AguaSabor 350ml',
-                        'Colombiana 350ml',
-                        'Manzana 350ml'
-                        ];
-                      
-  for (let i = 0; i < listaProductos.length; i++) {
+//----- ARRAY DE PRODUCTOS ----
+const BtnregistroProducto = document.querySelector('#BtnregistroProducto'); 
+//Esta es la declaración del array al cual estoy haciendo el push
+const listaProducto =[];
+
+BtnregistroProducto.addEventListener('click',()=>{
+  let codigo = 0;
+  let nombreProducto;
+  let presentacion;
+  let precio;
+  
+  //Este do while lo uso para tomar los datos del usuario a traves de los promt
+  do {
+    codigo++
+    nombreProducto = prompt('ingrese el nombre del producto. Para terminar escriba (salir)').toLowerCase().trim();
+    if (nombreProducto != 'salir') {
+      presentacion = prompt('ingrese la presentacion del producto.').toLowerCase().trim();
+      precio = prompt('ingrese el precio del producto.').toLowerCase().trim();
+    }else{
+      break;
+    }    
+    // el objeto donde almaceno los datos de los promt  
+    let registroProducto = {
+      codigo,
+      nombreProducto, 
+      presentacion,
+      precio
+    }
+    //lugar donde realizo el push para empujar el objeto hacia el arreglo
+    listaProducto.push(registroProducto);
     
+    //Una impresion de prueba para saber si el arreglo tiene el objeto
+    console.log('******');
+    console.log(listaProducto);
+    console.log('******');
+    //Uso este return que tengo entendido que asi puedo usar este arreglo por fuera de este bloque de codigo.
+    return listaProducto;
+  } while (nombreProducto != 'salir');
+  
+});
+
+//Intento manipular el arreglo e imprimir solo el valor del nombre del producto,hasta acá no logro entender porque no estoy capturando el arreglo.
+let prod;
+for(let producto of listaProducto){
+  prod = producto.nombreProducto;
+  console.log(prod);
+}
+
+//--- ARREGLO PARA IMPRIMIR EN PANTALLA LOS PRODUCTOS
+    // let listaProductos = ['Papas de pollo',
+    //                     'Papas de limon',
+    //                     'Papas de mayonesa',
+    //                     'Panecillos 10gr',
+    //                     'chetos de queso',
+    //                     'Coca-Cola 350ml',
+    //                     'Pepsi 350ml',
+    //                     'AguaSabor 350ml',
+    //                     'Colombiana 350ml',
+    //                     'Manzana 350ml'
+    //                     ];
+      
+
+
+  for (let i = 0; i < listaProducto.length; i++) {
+
+    console.log(listaProducto[i].codigo);
+    console.log(listaProducto[i].nombreProducto);
+
     const newElement = document.createElement('li');
-    newElement.textContent = listaProductos[i]; 
+    newElement.textContent = listaProducto[i]; 
     document.querySelector('#lista').appendChild(newElement);
+
+  
   }
+
 
 
 //--- FUNCION PARA CALCULAR EL COBRO E IMPRESION FACTURA
