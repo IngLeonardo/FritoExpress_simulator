8d74b4999d398e9080258cfe05431dbea4674f2c
IngLeonardo <8471301+IngLeonardo@users.noreply.github.com>
2024-04-22 22:28:05 -0500

    Se crea la rama entrega3 para presentar las nuevas moficaciones

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const productos = [
+  {
+    codigo: 1,
+    nombre: "Coca-Cola",
+    presentacion: "400ml",
+    precio: 2000,
+    img: "../img/coca400ml.png"
+  },
+  {
+    codigo: 2,
+    nombre: "Doritos",
+    presentacion: "200gr",
+    precio: 2200,
+    img: "../img/doritos.png"
+  },
+  {
+    codigo: 3,
+    nombre: "Hit",
+    presentacion: "500ml",
+    precio: 2800,
+    img: "../img/hit.png"
+  },
+  {
+    codigo: 4,
+    nombre: "Papas de pollo",
+    presentacion: "105gr",
+    precio: 3500,
+    img: "../img/papasPollo.png"
+  },
+  {
+    codigo: 5,
+    nombre: "De Todito",
+    presentacion: "Natural 400gr",
+    precio: 5000,
+    img: "../img/toditoNatural.png"
+  },
+  {
+    codigo: 6,
+    nombre: "Coca-Cola",
+    presentacion: "3litros",
+    precio: 8000,
+    img: "../img/coca3litros.png"
+  }
+]
+
+const vitrinaProductos = document.querySelector('#vitrinaProductos');
+
+productos.forEach((item)=>{
+
+  const newElemento = document.createElement('div');
+  newElemento.crea
+
+})
